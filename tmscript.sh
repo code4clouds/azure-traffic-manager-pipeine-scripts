446b958b3de81bc5ebb15cf26b4886c3d81ddfdc
Julio Colon <julio.colon@microsoft.com>
2018-03-05 15:41:01 -0500

    test params

--- tmscript.sh
+++ tmscript.sh
@@ -1,3 +1,6 @@
 echo Options received: $Enable
 echo Location received: $Location
-echo $params.REQUESTED_ACTION
+echo "$params.REQUESTED_ACTION" $params.REQUESTED_ACTION
+echo "$REQUESTED_ACTION" $REQUESTED_ACTION
+echo "${REQUESTED_ACTION}" ${REQUESTED_ACTION}
+echo "$(REQUESTED_ACTION)" $(REQUESTED_ACTION)
